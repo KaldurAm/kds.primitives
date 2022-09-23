473a2f51804339a9093233b7438c708e4ed12410
kalibek mergaziyev <kmergaziev@gmail.com>
2022-09-23 14:45:32 +0600

    [add] unit tests to check valueobject

--- tests/KDS.Primitives.UnitTests/AbstractionTests.cs
+++ tests/KDS.Primitives.UnitTests/AbstractionTests.cs
@@ -12,8 +12,28 @@ public class AbstractionTests
 	}
 	
 	[Fact]
-	public void TestClassNotInheritValueObject_Should_EqualFalse_EqualityOperatorFalse()
+	public void TestClassNotInheritValueObject_Should_EqualFalse_EqualityOperatorFalse_NotEqualTrue()
 	{
-		
+		TestClass testClass1 = new("Some data 1", "Some data 2");
+		TestClass testClass2 = new("Some data 1", "Some data 2");
+		var resultOfEqualMethod = testClass1.Equals(testClass2);
+		var resultOfEqualOperator = testClass1 == testClass2;
+		var resultOfNotEqualOperator = testClass1 != testClass2;
+		Assert.False(resultOfEqualMethod);
+		Assert.False(resultOfEqualOperator);
+		Assert.True(resultOfNotEqualOperator);
+	}
+
+	[Fact]
+	public void TestClassInheritValueObject_Should_EqualTrue_EqualityOperatorTrue_NotEqualFalse()
+	{
+		TestClassInheritValueObject testClass1 = new("Some data 1", "Some data 2");
+		TestClassInheritValueObject testClass2 = new("Some data 1", "Some data 2");
+		var resultOfEqualMethod = testClass1.Equals(testClass2);
+		var resultOfEqualOperator = testClass1 == testClass2;
+		var resultOfNotEqualOperator = testClass1 != testClass2;
+		Assert.True(resultOfEqualMethod);
+		Assert.True(resultOfEqualOperator);
+		Assert.False(resultOfNotEqualOperator);
 	}
 }
--- tests/KDS.Primitives.UnitTests/Usings.cs
+++ tests/KDS.Primitives.UnitTests/Usings.cs
@@ -1,4 +1,5 @@
 global using Xunit;
+using KDS.Primitives.Abstraction.ValueObjects;
 
 class TestClass
 {
@@ -11,5 +12,26 @@ class TestClass
 	{
 		TestString1 = testString1;
 		TestString2 = testString2;
+	}
+}
+
+class TestClassInheritValueObject : ValueObject
+{
+	public string TestString1 { get; set; }
+	public string TestString2 { get; set; }
+
+	public TestClassInheritValueObject() { }
+
+	public TestClassInheritValueObject(string testString1, string testString2)
+	{
+		TestString1 = testString1;
+		TestString2 = testString2;
+	}
+
+	/// <inheritdoc />
+	protected override IEnumerable<object> GetAtomicValues()
+	{
+		yield return TestString1;
+		yield return TestString2;
 	}
 }
